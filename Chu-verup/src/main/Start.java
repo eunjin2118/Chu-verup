d7c9107de6f6117a9f5689d2550b604cc19a05ad
은진 <80873499+eunjin2118@users.noreply.github.com>
2022-11-23 19:07:02 +0900

    시작 화면 생성

---
+++ Chu-verup/src/main/Start.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Start extends JFrame{
+	
+	// 배경
+	Image backgroundImage = new ImageIcon("src/image/startbackground.png").getImage();
+	// 버튼 이미지
+    ImageIcon startBtnImage = new ImageIcon("src/image/pepper.png");
+			
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
